adfc3bd9deb6165a737c39bcb59057978fd64d5a
anonymous <email>
2023-12-04 16:00:39 +0100

    creates: routes

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+
+function Main() {
+    return (
+        <>
+            <main>
+                <Routes>
+                    <Route path="/"/>
+                    <Route path="/books"/>
+                    <Route path="/register"/>
+                    <Route path="/login"/>
+                    <Route path="/profile"/>
+                    <Route path="/cart"/>
+                    <Route path="*"/>
+                </Routes>
+            </main>
+        </>
+    );
+};
+
+export default Main;
