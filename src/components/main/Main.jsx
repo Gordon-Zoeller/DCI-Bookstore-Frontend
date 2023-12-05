2950766ad098ef81fd08ed1bdbf9ff9d026e5324
pozniej-znajde-wolne-haslo <aniarafeld@o2.pl>
2023-12-05 13:28:17 +0100

    adds LOGOUT function

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,24 +1,24 @@
 import { Routes, Route } from 'react-router-dom';
 import Register from '../pages/register/Register';
-import Login from '../pages/register/Login';
+import Login from '../pages/login/Login';
 
 function Main() {
-    return (
-        <>
-            <main>
-                <Routes>
-                    <Route path="/"/>
-                    <Route path="/books"/>
-                    <Route path="/books/:search"/>
-                    <Route path='/login' element={<Login />} />
-                    <Route path="/register" element={<Register />}/>
-                    <Route path="/profile"/>
-                    <Route path="/cart"/>
-                    <Route path="*"/>
-                </Routes>
-            </main>
-        </>
-    );
-};
+  return (
+    <>
+      <main>
+        <Routes>
+          <Route path='/' />
+          <Route path='/books' />
+          <Route path='/books/:search' />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/profile' />
+          <Route path='/cart' />
+          <Route path='*' />
+        </Routes>
+      </main>
+    </>
+  );
+}
 
 export default Main;
